ad8c2121f9126bb7bf02d0f97af84ad8161960a5
Marcus Battle <marcusbattle@marcbook.attlocal.net>
2018-01-13 23:48:02 -0500

    Fixed giving in the navigation

--- themes/mop-2015/views/navigation.php
+++ themes/mop-2015/views/navigation.php
@@ -13,7 +13,7 @@
 		<h1 id="logo"><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/monument-logo-white.png" width="194" height="80" /></a></h1>
 		<ul id="location-menu" class="nav nav-pills pull-right">
 			<li class="hidden-xs" role="presentation"><a href="<?php echo home_url('watch'); ?>">Watch Live</a></li>
-			<li class="visible-xs" role="presentation"><a href="<?php echo home_url('give'); ?>">Give</a></li>
+			<li class="hidden-xs" role="presentation"><a href="<?php echo home_url('give'); ?>">Give</a></li>
 			<li class="hidden-xs" role="presentation"><a href="<?php echo home_url('events'); ?>">Events</a></li>
 			<li class="hidden-xs" role="presentation"><a href="<?php echo home_url('our-story'); ?>">Our Story</a></li>
 			<li role="presentation"><a href="#" class="open-menu">Menu <i class="fa fa-bars"></i></a></li>
